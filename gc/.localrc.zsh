4691ee973e0833390dcd2987ab975d99ec595573
Dominik Rys <dominik.j.rys@gmail.com>
2024-12-30 01:38:16 +0000

    Add ghostty

--- gc/.localrc.zsh
+++ gc/.localrc.zsh
@@ -1,18 +1,18 @@
-# Remove nvm prom p10k
+# Remove nvm from p10k
 typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    status                  # exit code of the last command
-    command_execution_time  # duration of the last command
-    background_jobs         # presence of background jobs
-    direnv                  # direnv status (https://direnv.net/)
-    asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-    anaconda                # conda environment (https://conda.io/)
-    pyenv                   # python environment (https://github.com/pyenv/pyenv)
-    goenv                   # go environment (https://github.com/syndbg/goenv)
-    nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
+    status                 # exit code of the last command
+    command_execution_time # duration of the last command
+    background_jobs        # presence of background jobs
+    direnv                 # direnv status (https://direnv.net/)
+    asdf                   # asdf version manager (https://github.com/asdf-vm/asdf)
+    virtualenv             # python virtual environment (https://docs.python.org/3/library/venv.html)
+    anaconda               # conda environment (https://conda.io/)
+    pyenv                  # python environment (https://github.com/pyenv/pyenv)
+    goenv                  # go environment (https://github.com/syndbg/goenv)
+    nodenv                 # node.js version from nodenv (https://github.com/nodenv/nodenv)
     # nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
-    nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
+    nodeenv # node.js environment (https://github.com/ekalinin/nodeenv)
     # node_version          # node.js version
     # go_version            # go version (https://golang.org)
     # rust_version          # rustc version (https://www.rust-lang.org)
@@ -21,41 +21,41 @@ typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # laravel_version       # laravel php framework version (https://laravel.com/)
     # java_version          # java version (https://www.java.com/)
     # package               # name@version from package.json (https://docs.npmjs.com/files/package.json)
-    rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
-    rvm                     # ruby version from rvm (https://rvm.io)
-    fvm                     # flutter version management (https://github.com/leoafarias/fvm)
-    luaenv                  # lua version from luaenv (https://github.com/cehoffman/luaenv)
-    jenv                    # java version from jenv (https://github.com/jenv/jenv)
-    plenv                   # perl version from plenv (https://github.com/tokuhirom/plenv)
-    phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
-    scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
-    haskell_stack           # haskell version from stack (https://haskellstack.org/)
-    kubecontext             # current kubernetes context (https://kubernetes.io/)
-    terraform               # terraform workspace (https://www.terraform.io)
-    aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
-    aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
-    azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
-    gcloud                  # google cloud cli account and project (https://cloud.google.com/)
-    google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
-    context                 # user@hostname
-    nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
-    ranger                  # ranger shell (https://github.com/ranger/ranger)
-    nnn                     # nnn shell (https://github.com/jarun/nnn)
-    vim_shell               # vim shell indicator (:sh)
-    midnight_commander      # midnight commander shell (https://midnight-commander.org/)
-    nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
+    rbenv              # ruby version from rbenv (https://github.com/rbenv/rbenv)
+    rvm                # ruby version from rvm (https://rvm.io)
+    fvm                # flutter version management (https://github.com/leoafarias/fvm)
+    luaenv             # lua version from luaenv (https://github.com/cehoffman/luaenv)
+    jenv               # java version from jenv (https://github.com/jenv/jenv)
+    plenv              # perl version from plenv (https://github.com/tokuhirom/plenv)
+    phpenv             # php version from phpenv (https://github.com/phpenv/phpenv)
+    scalaenv           # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
+    haskell_stack      # haskell version from stack (https://haskellstack.org/)
+    kubecontext        # current kubernetes context (https://kubernetes.io/)
+    terraform          # terraform workspace (https://www.terraform.io)
+    aws                # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
+    aws_eb_env         # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
+    azure              # azure account name (https://docs.microsoft.com/en-us/cli/azure)
+    gcloud             # google cloud cli account and project (https://cloud.google.com/)
+    google_app_cred    # google application credentials (https://cloud.google.com/docs/authentication/production)
+    context            # user@hostname
+    nordvpn            # nordvpn connection status, linux only (https://nordvpn.com/)
+    ranger             # ranger shell (https://github.com/ranger/ranger)
+    nnn                # nnn shell (https://github.com/jarun/nnn)
+    vim_shell          # vim shell indicator (:sh)
+    midnight_commander # midnight commander shell (https://midnight-commander.org/)
+    nix_shell          # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vi_mode               # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
     # ram                   # free RAM
     # swap                  # used swap
-    todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
-    timewarrior             # timewarrior tracking status (https://timewarrior.net/)
-    taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    time                    # current time
+    todo        # todo items (https://github.com/todotxt/todo.txt-cli)
+    timewarrior # timewarrior tracking status (https://timewarrior.net/)
+    taskwarrior # taskwarrior task count (https://taskwarrior.org/)
+    time        # current time
     # =========================[ Line #2 ]=========================
-    newline                 # \n
+    newline # \n
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
@@ -84,11 +84,6 @@ export PATH="$HOME/.local/bin:$PATH"
 # Add bin to path for downloaded binaries
 export PATH="$HOME/bin:$PATH"
 
-# Configure setting monitor settings
-alias dhome='displayplacer "id:ABD7749C-8F4E-4F74-9863-AADC697B1A90 res:2560x1440 hz:60 color_depth:7 scaling:on origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 scaling:on origin:(2560,422) degree:0"'
-alias doffice='displayplacer "id:A8C53458-F651-4179-ABFE-B9E10A8337A3 res:2560x1440 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 scaling:on origin:(452,1440) degree:0"'
-alias doffice2='displayplacer "id:5E3FAA22-A8AE-4D91-8D80-FB6AB9132F0F res:2560x1440 hz:60 color_depth:7 enabled:true scaling:on origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 enabled:true scaling:on origin:(107,1440) degree:0'
-
 # Configure GKE auth plugin for gcloud
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 
@@ -106,3 +101,24 @@ export PATH="/Users/drys/.asdf/shims:$PATH"
 
 # Add extra paths to path to use updated bash
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
+# Configure Rancher desktop
+export PATH="$HOME/.rd/bin:$PATH"
+export DOCKER_HOST="unix://$HOME/.rd/docker.sock"
+
+# Log in to Google and restart Docker
+restart-docker() {
+    echo "==> Triggering gcloud login flow..."
+    utopia config setup --google-credentials
+
+    echo "==> Restarting Rancher Desktop..."
+    rdctl shutdown && rdctl start
+
+    echo "==> Waiting for Docker to start..."
+    while (! docker stats --no-stream &>/dev/null); do
+        sleep 1
+    done
+}
+
+# Alias utopia commands
+alias reauth='utopia config setup --google-credentials'
